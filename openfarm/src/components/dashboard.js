f304851615b719cc859edbb0355b54e38b50e932
maybel66 <ogirikimabel@gmail.com>
2018-03-19 17:31:12 +0100

    added the top bar"

--- openfarm/src/components/dashboard.js
+++ openfarm/src/components/dashboard.js
@@ -1,14 +1,47 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
 import '../App.css';
 
 export default class Dashboard extends Component {
   render() {
     return (
       <div className="App">
-        <p className="App-intro">
-          This is the dashboard
-        </p>
+        <div className='row'>
+          <div className='col-sm-3' style={{fontSize:20}}>
+            <div className='row'>
+              <div className='panel panel-default' style={{backgroundColor:'grey'}}>
+                <div className='panel-body'>
+                  <div className='col-sm-6'>hello there</div>
+                  <div className='col-sm-6'>hello</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='col-sm-9' style={{fontSize:20}}>
+            <div className='row'>
+              <div className='panel panel-default' style={{backgroundColor:'lightgrey'}}>
+                <div className='panel-body'>
+                  <div className='col-sm-5' style={{backgroundColor:'yellow'}}>hello there</div>
+                  <div className='col-sm-7' style={{backgroundColor:'red'}}>
+                      <div className='row'>
+                        <div className='col-sm-3'>hello</div>
+                        <div className='col-sm-1' >hey</div>
+                        <div className='col-sm-2'>
+                        <div className='column'>
+                        
+                        </div>
+                       </div>
+                        <div className='col-sm-2' >hello</div>
+                        <div className='col-sm-2' >hello</div>
+                        <div className='col-sm-2' >hello
+                        </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
       </div>
     );
   }
