8ffad57d8d2fa5849535fe027f89f7dfebc2d82f
Richard Igbiriki <imorobebh@gmail.com>
2018-03-19 19:28:44 +0100

    header completed for non-signed in users

--- openfarm/src/App.js
+++ openfarm/src/App.js
@@ -1,89 +1,99 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom'
 import logo from './openLogo.png';
+import first from './images/third.jpg';
+import second from './images/second.jpg';
+import third from './images/fourth.jpg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className='row hidden-xs' style={{backgroundColor:'#FAFAFA',border: '0.5px solid lightgrey', }}>
-          <div className='row' style={{margin:10}}>
-            <div className='col-md-3 col-sm-3'>
-              <div>
-                <img src={logo} alt="logo" style={{height:50, width:100}} />
+      <div className="App" style={{marginTop:-20}}>
+        <div id="myCarousel" className="carousel slide" data-ride="carousel">
+          <ol className="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+          </ol>
+          <div className="carousel-inner">
+            <div className="item active">
+                <img src={first} alt="Los Angeles" style={{width:'100%', height:'500px' }} />
+              <div class="carousel-caption" style={{top:'50%'}}>
+                <h2 style={{fontWeight:'600'}}>Connect with Farmers,</h2>
+                <h2 style={{fontWeight:'600'}}>Funding, and Markets</h2>
+                <button style={{backgroundColor:'#1babc7', padding:10, color:'white', borderColor:'transparent', borderRadius:2}}>GET STARTED</button>
               </div>
             </div>
-            <div className='col-md-3 col-sm-3'>
-              <div className='row'>
-                <div className='col-sm-3' style={{marginRight:-20}}>
-                  <div className='pull-right'>
-                    <i className='fas fa-map-marker-alt' style={{color:'#069fba'}}></i>
-                  </div>
-                </div>
-                <div className='col-sm-9' style={{fontSize:10, lineHeight:1, lineSpace:1}}>
-                  <div className='pull-left'>
-                    <p>11, Elekahia Road</p>
-                    <p>Port Harcourt</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className='col-md-3 col-sm-3'>
-              <div className='row'>
-                <div className='col-sm-3 text-center'>
-                  <div className='pull-right'>
-                    <i className="fas fa-copy" style={{color:'#069fba'}}></i>
-                  </div>
-                </div>
-                <div className='col-sm-9' style={{marginLeft:-20}}>
-                  <div className='pull-left' style={{fontSize:10, lineHeight:1, lineSpace:1}}>
-                    <p>info@openfarm.com.ng</p>
-                    <span>+234 809 773 7457</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className='col-md-3 col-sm-3'>
-              <div className='pull-right'>
-                <button className='btn btn-primary' style={{backgroundColor:'#069fba',borderColor:'transparent'}}>
-                  REGISTER/LOGIN
-                </button>
-              </div>
 
+            <div className="item">
+              <img src={second} alt="Chicago" style={{width:'100%', height:'500px'}} />
+              <div class="carousel-caption" style={{top:'50%'}}>
+                <h2 style={{fontWeight:'600'}}>Connect with Farmers,</h2>
+                <h2 style={{fontWeight:'600'}}>Funding, and Markets</h2>
+                <button style={{backgroundColor:'#1babc7', padding:10, color:'white', borderColor:'transparent', borderRadius:2}}>GET STARTED</button>
+              </div>
+            </div>
+            <div className="item">
+              <img src={third} alt="New York" style={{width:'100%', height:'500px'}} />
+              <div class="carousel-caption" style={{top:'50%'}}>
+                <h2 style={{fontWeight:'600'}}>Connect with Farmers,</h2>
+                <h2 style={{fontWeight:'600'}}>Funding, and Markets</h2>
+                <button style={{backgroundColor:'#1babc7', padding:10, color:'white', borderColor:'transparent', borderRadius:2}}>GET STARTED</button>
+              </div>
             </div>
           </div>
-      </div>
-      <nav className="navbar navbar-default" style={{backgroundColor:'#069fba'}}>
-  <div className="container-fluid" style={{marginLeft:'8%'}}>
-    <div className="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span className="sr-only">Toggle navigation</span>
-        <span className="icon-bar"></span>
-        <span className="icon-bar"></span>
-        <span className="icon-bar"></span>
-      </button>
-      <a style={{color:'white'}} className="navbar-brand" href="#">HOME</a>
-    </div>
-    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul className="nav navbar-nav">
-        <li><a href="#" style={{color:'white'}}>FUNDING <span className="sr-only">(current)</span></a></li>
-        <li><a href="#" style={{color:'white'}}>TRAINING</a></li>
-        <li><a href="#" style={{color:'white'}}>NEWS</a></li>
-        <li><a href="#" style={{color:'white'}}>ABOUT US</a></li>
-        <li><a href="#" style={{color:'white'}}>CONTACT US</a></li>
-      </ul>
-
-      <ul className="nav navbar-nav navbar-right" style={{backgroundColor:'#1b9fb9'}}>
-        <form className="navbar-form navbar-left">
-          <div className="form-group">
-            <input type="text"  className="form-control" placeholder="Search" style={{backgroundColor:'transparent', color:'white'}} />
+          <a className="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span className="glyphicon glyphicon-chevron-left"></span>
+            <span className="sr-only">Previous</span>
+          </a>
+          <a className="right carousel-control" href="#myCarousel" data-slide="next">
+            <span className="glyphicon glyphicon-chevron-right"></span>
+            <span className="sr-only">Next</span>
+          </a>
+        </div>
+        <div className='row'>
+          <div className='col-sm-12'>
+            <h2 className='lead' style={{fontWeight:'600'}}>How it Works</h2>
           </div>
-        </form>
-      </ul>
-
-    </div>
-  </div>
-</nav>
+          <div className='col-sm-12' style={{fontWeight:'500', fontSize:'20'}}>
+            <div className='col-sm-3'>
+              <div className='row' style={{border:'1px solid lightgrey', margin:10}}>
+                <img src={require('./images/register-icon.svg')} style={{padding:20, height:100, width:100}} />
+              </div>
+              <p style={{fontWeight:'600', fontSize:'16'}}>REGISTER A PROFILE</p>
+              <p>Register with OpenFarm to start your journey towards a more efficient farming experience.</p>
+            </div>
+            <div className='col-sm-3'>
+              <div className='row' style={{border:'1px solid lightgrey', margin:10}}>
+              <img src={require('./images/list-produce-icon.svg')} style={{padding:20, height:100, width:100}} />
+            </div>
+              <p style={{fontWeight:'600', fontSize:'16'}}>LIST YOUR PRODUCE</p>
+              <p>List your products, and get buyers/investors.</p>
+            </div>
+            <div className='col-sm-3'>
+              <div className='row' style={{border:'1px solid lightgrey', margin:10}}>
+                <img src={require('./images/access-fund-icon.svg')} style={{padding:20, height:100, width:100}}  />
+              </div>
+              <p style={{fontWeight:'600', fontSize:'16'}}>ACCESS FUNDING</p>
+              <p>Gain access to loans, and grants offered by individuals or corporate bodies.</p>
+            </div>
+            <div className='col-sm-3'>
+              <div className='row' style={{border:'1px solid lightgrey', margin:10}}>
+              <img src={require('./images/connect-icon.svg')} style={{padding:20, height:100, width:100}}  />
+              </div>
+              <p style={{fontWeight:'600', fontSize:'16'}}>CONNECT & NETWORK</p>
+              <p>Meet and connect with farmers, investors, and veterinarians with just a click </p>
+            </div>
+          </div>
+          <div className='container'>
+            <div className='row'>
+              <div className='row'>
+                <img src={require('./images/tiny-wheel.svg')} style={{padding:20, height:60, width:60}}  />
+              </div>
+            </div>
+          </div>
+        </div>
         </div>
     );
   }
--- openfarm/src/components/header.js
+++ openfarm/src/components/header.js
@@ -1,15 +1,95 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
+import {Link} from 'react-router-dom'
+import logo from '../openLogo.png';
 import '../App.css';
 
 export default class Header extends Component {
+  constructor (props) {
+    super(props)
+      this.state =  {
+        selected:'home'
+      }
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <div className='row hidden-xs' style={{backgroundColor:'#FAFAFA',border: '0.5px solid lightgrey', }}>
+          <div className='row' style={{margin:10}}>
+            <div className='col-md-3 col-sm-3'>
+              <div>
+                <img src={logo} alt="logo" style={{height:50, width:100}} />
+              </div>
+            </div>
+            <div className='col-md-3 col-sm-3'>
+              <div className='row'>
+                <div className='col-sm-3' style={{marginRight:-20}}>
+                  <div className='pull-right'>
+                    <i className='fas fa-map-marker-alt' style={{color:'#069fba'}}></i>
+                  </div>
+                </div>
+                <div className='col-sm-9' style={{fontSize:10, lineHeight:1, lineSpace:1}}>
+                  <div className='pull-left'>
+                    <p>11, Elekahia Road</p>
+                    <p>Port Harcourt</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='col-md-3 col-sm-3'>
+              <div className='row'>
+                <div className='col-sm-3 text-center'>
+                  <div className='pull-right'>
+                    <i className="fas fa-copy" style={{color:'#069fba'}}></i>
+                  </div>
+                </div>
+                <div className='col-sm-9' style={{marginLeft:-20}}>
+                  <div className='pull-left' style={{fontSize:10, lineHeight:1, lineSpace:1}}>
+                    <p>info@openfarm.com.ng</p>
+                    <span>+234 809 773 7457</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='col-md-3 col-sm-3'>
+              <div className='pull-right'>
+                <button className='btn btn-primary' style={{backgroundColor:'#069fba',borderColor:'transparent'}}>
+                  REGISTER/LOGIN
+                </button>
+              </div>
+
+            </div>
+          </div>
+      </div>
+      <nav className="navbar navbar-default" style={{backgroundColor:'#069fba'}}>
+  <div className="container-fluid" style={{marginLeft:'8%'}}>
+    <div className="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span className="sr-only">Toggle navigation</span>
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+      </button>
+    </div>
+    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul className="nav navbar-nav">
+        <li onClick={()=>this.setState({selected:'home'})}> <Link  to='/' style={{color: this.state.selected === 'home' ? 'black' :'white'}}>HOME <span className="sr-only">(current)</span></Link></li>
+        <li onClick={()=>this.setState({selected:'funding'})}><Link to="/funding" style={{color:this.state.selected === 'funding' ? 'black' :'white'}}>FUNDING <span className="sr-only">(current)</span></Link></li>
+        <li onClick={()=>this.setState({selected:'training'})}><Link to="/training" style={{color:this.state.selected === 'training' ? 'black' :'white'}}>TRAINING</Link></li>
+        <li onClick={()=>this.setState({selected:'news'})}><Link to="/news" style={{color:this.state.selected === 'news' ? 'black' :'white'}}>NEWS</Link></li>
+        <li onClick={()=>this.setState({selected:'about'})}><Link to="/about" style={{color:this.state.selected === 'about' ? 'black' :'white'}}>ABOUT US</Link></li>
+        <li onClick={()=>this.setState({selected:'contact'})}><Link to="/contact" style={{color:this.state.selected === 'contact' ? 'black' :'white'}}>CONTACT US</Link></li>
+      </ul>
+      <ul className="nav navbar-nav navbar-right" style={{backgroundColor:'#007185'}}>
+        <form className="navbar-form navbar-left">
+          <div className="form-group">
+            <input type="text"  className="form-control" placeholder="Search" style={{backgroundColor:'transparent', color:'white', border:'0px solid transparent'}} />
+          </div>
+        </form>
+      </ul>
+
+    </div>
+  </div>
+</nav>
         {this.props.children}
       </div>
     );
