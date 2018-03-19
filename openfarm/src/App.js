19d03a53187cb96c8b7f72814082733a59e6bea8
Richard Igbiriki <imorobebh@gmail.com>
2018-03-19 17:40:20 +0100

    header started

--- openfarm/src/App.js
+++ openfarm/src/App.js
@@ -1,54 +1,90 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './openLogo.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className='row xs-hidden text-center' style={{backgroundColor:'lightgrey'}}>
-        <div className='col-md-3'>Open Farm</div>
-        <div className='col-md-3'>
-          <div className='row'>
-            <div className='col-sm-3'>
+        <div className='row hidden-xs' style={{backgroundColor:'#FAFAFA',border: '0.5px solid lightgrey', }}>
+          <div className='row' style={{margin:10}}>
+            <div className='col-md-3 col-sm-3'>
+              <div>
+                <img src={logo} alt="logo" style={{height:50, width:100}} />
+              </div>
+            </div>
+            <div className='col-md-3 col-sm-3'>
+              <div className='row'>
+                <div className='col-sm-3' style={{marginRight:-20}}>
+                  <div className='pull-right'>
+                    <i className='fas fa-map-marker-alt' style={{color:'#069fba'}}></i>
+                  </div>
+                </div>
+                <div className='col-sm-9' style={{fontSize:10, lineHeight:1, lineSpace:1}}>
+                  <div className='pull-left'>
+                    <p>11, Elekahia Road</p>
+                    <p>Port Harcourt</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='col-md-3 col-sm-3'>
+              <div className='row'>
+                <div className='col-sm-3 text-center'>
+                  <div className='pull-right'>
+                    <i className="fas fa-copy" style={{color:'#069fba'}}></i>
+                  </div>
+                </div>
+                <div className='col-sm-9' style={{marginLeft:-20}}>
+                  <div className='pull-left' style={{fontSize:10, lineHeight:1, lineSpace:1}}>
+                    <p>info@openfarm.com.ng</p>
+                    <span>+234 809 773 7457</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='col-md-3 col-sm-3'>
               <div className='pull-right'>
-                <i className='fa fa-map pull-right'></i>
+                <button className='btn btn-primary' style={{backgroundColor:'#069fba',borderColor:'transparent'}}>
+                  REGISTER/LOGIN
+                </button>
               </div>
-            </div>
-            <div className='col-sm-9'>
-              <div className='pull-left'>
-                <p>11, Elekahia Road</p>
-                <p>Port Harcourt</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className='col-md-3'>
-          <div className='row'>
-            <div className='col-sm-3' style={{backgroundColor:'yellow'}}>
-              <div className='pull-right'>
-                <i className='fa fa-envelope pull-right'></i>
-              </div>
-            </div>
-            <div className='col-sm-9' style={{backgroundColor:'red'}}>
-              <div className='pull-left'>
-                <p>info@openfarm.com.ng</p>
-                <span>+234 809 773 7457</span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className='col-md-3'>
-          <button className='btn btn-primary' style={{backgroundColor:'#069fba',borderColor:'transparent'}}>
-            REGISTER/LOGIN
-          </button>
-        </div>
-      </div>
 
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+            </div>
+          </div>
       </div>
+      <nav className="navbar navbar-default" style={{backgroundColor:'#069fba'}}>
+  <div className="container-fluid" style={{marginLeft:'8%'}}>
+    <div className="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span className="sr-only">Toggle navigation</span>
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+      </button>
+      <a style={{color:'white'}} className="navbar-brand" href="#">HOME</a>
+    </div>
+    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul className="nav navbar-nav">
+        <li><a href="#" style={{color:'white'}}>FUNDING <span className="sr-only">(current)</span></a></li>
+        <li><a href="#" style={{color:'white'}}>TRAINING</a></li>
+        <li><a href="#" style={{color:'white'}}>NEWS</a></li>
+        <li><a href="#" style={{color:'white'}}>ABOUT US</a></li>
+        <li><a href="#" style={{color:'white'}}>CONTACT US</a></li>
+      </ul>
+
+      <ul className="nav navbar-nav navbar-right" style={{backgroundColor:'#1b9fb9'}}>
+        <form className="navbar-form navbar-left">
+          <div className="form-group">
+            <input type="text"  className="form-control" placeholder="Search" style={{backgroundColor:'transparent', color:'white'}} />
+          </div>
+        </form>
+      </ul>
+
+    </div>
+  </div>
+</nav>
+        </div>
     );
   }
 }
