02026e2b25d22783189eff00f8c46ee2a298a804
Richard Igbiriki <imorobebh@gmail.com>
2018-03-19 20:36:53 +0100

    merged with home

---
+++ openfarm/src/helpers/firebase.js
@@ -0,0 +1,10 @@
+import * as firebase from 'firebase'
+  var config = {
+    apiKey: "AIzaSyC1-b5TmqmShzUDGC3ChneysQg2nZj9fo0",
+    authDomain: "openfarm-db746.firebaseapp.com",
+    databaseURL: "https://openfarm-db746.firebaseio.com",
+    projectId: "openfarm-db746",
+    storageBucket: "",
+    messagingSenderId: "601405019635"
+  }
+  export const Firebase = firebase.initializeApp(config)
